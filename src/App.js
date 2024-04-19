1d7788b5413f36a9be4a2e870d48f0f9c4fe22b8
Param013 <paramgodhani9@gmail.com>
2024-04-19 20:46:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // App.js
-
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,25 @@
 import React from 'react';
 
 const About = () => {
-  const languages = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Next.js",
-    "C++",
-    "C",
-    "Python",
-    "RStudio",
-    "Node.js",
-    "Java",
-    "Typescript"
+
+  const link = [
+    {url:"https://www.w3schools.com/html/default.asp", label: 'HTML'},
+    {url:"https://www.w3schools.com/css/default.asp", label: 'CSS'},
+    {url:"https://www.w3schools.com/js/default.asp", label: 'JavaScript'},
+    {url:"https://www.w3schools.com/react/default.asp", label: 'React'},
+    {url:"https://www.geeksforgeeks.org/next-js-introduction/", label: 'Next.js'},
+    {url:"https://www.w3schools.com/js/default.asp", label: 'Node.js'},
+    {url:"https://www.w3schools.com/cpp/default.asp", label: 'C++'},
+    {url:"https://www.w3schools.com/c/index.php", label: 'C'},
+    {url:"https://www.w3schools.com/python/default.asp", label: 'Python'},
+    {url:"https://www.w3schools.com/r/default.asp", label: 'RStudio'},
+    {url:"https://www.w3schools.com/java/default.asp", label: 'Java'},
+    {url:"https://www.w3schools.com/typescript/index.php", label: 'Typescript'},
   ];
+
+  const links = (url) => {
+    window.location.href=url;
+  }
 
   return (
     <section id="about">
@@ -24,15 +29,12 @@ const About = () => {
         <h2>About Me</h2>
         <p>I am a passionate developer with expertise in:</p>
         <div className="languages-container">
-          {languages.map((language, index) => (
-            <div key={index} className="language-card">
-              {language}
-            </div>
-          ))}
+        {link.map((link,index)=>  
+          (<button key="index"className="language-card" onClick={()=>links(link.url)}>
+            {link.label}</button>))}
         </div>
         <br></br>
       </div>
-      {/* <a className="Resumebtn" href="Resume.html">Resume</a> */}
     </section>
   );
 }
